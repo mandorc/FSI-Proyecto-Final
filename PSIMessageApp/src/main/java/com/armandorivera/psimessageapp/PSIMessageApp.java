5db59153134ebee8e28ac60c02faec38b0d17502
Armando <95893860+mandorc@users.noreply.github.com>
2025-12-04 14:45:24 -0600

    Maven Created

---
+++ PSIMessageApp/src/main/java/com/armandorivera/psimessageapp/PSIMessageApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.armandorivera.psimessageapp;
+
+/**
+ *
+ * @author armando
+ */
+public class PSIMessageApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
